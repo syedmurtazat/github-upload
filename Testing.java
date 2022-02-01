288ec7ff689ed5488c080c52bad6068513ded351
Syed M <syedmurtazat@gmail.com>
2022-02-01 16:16:11 -0600

    initializing repository

---
+++ Testing.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Testing {
+
+
+    public static void main (String[] args)
+    {
+        int[] str = new int[3];
+
+        int x = 10;
+
+        for(int idx = 0; idx < str.length; idx++)
+        {
+            str[idx] = x;
+            System.out.println(str);
+        }
+
+
+    }
+}
